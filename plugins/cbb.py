1d34ce9f4b2c19790cb0c32644bd3448c2f2c9ee
SMK-BOTZZ <161198246+SMK-BOTZZ@users.noreply.github.com>
2024-04-02 18:22:34 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href="https://t.me/SHUBHAM_X_OFFICIAL">♡ Shubham ♡</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Channel : @SMKTHEBOTZ\n○ Support Group : @SMKSUPPORTGROUP</b>",
+            text = f"<b>○ Creator : <a href='tg://user?id={OWNER_ID}'>♡ Shubham ♡</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://t.me/SMKTHEBOTZ'>Click here</a>\n○ Channel : @SMKTHEBOTZ\n○ Support Group : @SMKSUPPORTGROUP</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
